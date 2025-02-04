713d78bc2534c655320a904f2410e32bba9b25a1
Steven Bias <bias_steven@hotmail.fr>
2025-02-04 23:36:26 +0100

    No do while in zig, use while(true) instead, it is not 'ugly' for zig president...
    See: https://github.com/ziglang/zig/issues/2159\#issuecomment-478635734

--- guess.zig
+++ guess.zig
@@ -4,13 +4,16 @@ const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
 
 pub fn main() !void {
-    const rand = std.crypto.random.uintLessThanBiased(u8, 100);
-    try stdout.print("{s} ", .{"Enter a number:"});
+    const rand = std.crypto.random.uintLessThanBiased(u8, 101);
 
-    var input_number: [10]u8 = undefined;
-    const in_num = try stdin.readUntilDelimiter(&input_number, '\n');
-    const number: u8 = std.fmt.parseInt(u8, in_num, 10) catch 0;
+    while (true) {
+        try stdout.print("\n{s} ", .{"Enter a number between 0 and 100:"});
+        var input_number: [10]u8 = undefined;
+        const in_num = try stdin.readUntilDelimiter(&input_number, '\n');
+        const number: u8 = std.fmt.parseInt(u8, in_num, 10) catch 0;
 
-    try stdout.print("\nYour number is: {}!\n", .{number});
-    try stdout.print("My number is: {}!\n", .{rand});
+        try stdout.print("\nYour number is: {}!\n", .{number});
+        try stdout.print("My number is: {}!\n", .{rand});
+        break;
+    }
 }
