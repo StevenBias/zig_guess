e2fbef94d73068b142bc4f372f38465ec75c1ad4
Steven Bias <bias_steven@hotmail.fr>
2025-02-04 22:33:43 +0100

    Get an number from stdin and print it to stdout

--- guess.zig
+++ guess.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
 
 const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
 pub fn main() !void {
-    try stdout.print("{s}\n", .{"Hello!"});
+
+    try stdout.print("{s} ", .{"Enter a number:"});
+
+    var input_number: [10]u8 = undefined;
+    const in_num = try stdin.readUntilDelimiter(&input_number, '\n');
+    const number: u8 = std.fmt.parseInt(u8, in_num, 10) catch 0;
+
+    try stdout.print("\nYour number is: {}!\n", .{number});
 }
