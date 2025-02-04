966ed08d63d8ae5cb668b116ef214849d2a89e9b
Steven Bias <bias_steven@hotmail.fr>
2025-02-04 23:38:45 +0100

    First version of the game

--- guess.zig
+++ guess.zig
@@ -8,12 +8,24 @@ pub fn main() !void {
 
     while (true) {
         try stdout.print("\n{s} ", .{"Enter a number between 0 and 100:"});
+
         var input_number: [10]u8 = undefined;
         const in_num = try stdin.readUntilDelimiter(&input_number, '\n');
-        const number: u8 = std.fmt.parseInt(u8, in_num, 10) catch 0;
+        const number = std.fmt.parseInt(u8, in_num, 10) catch 101;
 
-        try stdout.print("\nYour number is: {}!\n", .{number});
-        try stdout.print("My number is: {}!\n", .{rand});
-        break;
+        if (number > 100) {
+            try stdout.print("\n{s}\n", .{"Wrong number, please retry."});
+            continue;
+        }
+
+        if (rand < number) {
+            try stdout.print("It is {s}!\n", .{"lower"});
+        } else if (rand > number) {
+            try stdout.print("It is {s}!\n", .{"higher"});
+        } else {
+            try stdout.print("\n{s}\n", .{"You win!"});
+            break;
+        }
     }
+    try stdout.print("\nThe secret number is: {}!\n", .{rand});
 }
