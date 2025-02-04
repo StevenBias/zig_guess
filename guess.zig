99caf8da624e58ff00d2e47221e1bbb184328454
Steven Bias <bias_steven@hotmail.fr>
2025-02-04 22:42:47 +0100

    Create random uint less than 100 and print it

--- guess.zig
+++ guess.zig
@@ -4,7 +4,7 @@ const stdout = std.io.getStdOut().writer();
 const stdin = std.io.getStdIn().reader();
 
 pub fn main() !void {
-
+    const rand = std.crypto.random.uintLessThanBiased(u8, 100);
     try stdout.print("{s} ", .{"Enter a number:"});
 
     var input_number: [10]u8 = undefined;
@@ -12,4 +12,5 @@ pub fn main() !void {
     const number: u8 = std.fmt.parseInt(u8, in_num, 10) catch 0;
 
     try stdout.print("\nYour number is: {}!\n", .{number});
+    try stdout.print("My number is: {}!\n", .{rand});
 }
